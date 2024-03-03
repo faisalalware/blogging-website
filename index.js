331c7789668f38183d3086ce2c43d923b80b48b5
Faisal Alware <85944127+faisalalware@users.noreply.github.com>
2024-03-03 17:22:49 +0530

    handled navbar on signin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const userRoute = require("./routes/user");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -12,9 +14,13 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get('/', (req, res) => {
-    res.render("home");
+    res.render("home", {
+        user: req.user,
+    });
 });
 
 app.use('/user', userRoute);
