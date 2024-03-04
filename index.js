b7b05a154ddcda1e614b694375089ac5ea2585db
Faisal Alware <85944127+faisalalware@users.noreply.github.com>
2024-03-04 20:36:54 +0530

    read blog functionality completed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const express = require("express");
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+const Blog = require('./models/blog')
 
 const app = express();
 const PORT = 8000;
@@ -16,13 +19,17 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find({}).populate("createdBy");
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
 app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
